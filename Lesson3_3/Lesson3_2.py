bf82720557cc41d699eb22240cc23f58f9f10055
Netology_Pyhon <nlpythonghub@gmail.com>
2017-07-14 02:08:32 +0300

    Fixed code agreed with comments

--- Lesson3_3/Lesson3_2.py
+++ Lesson3_3/Lesson3_2.py
@@ -2,21 +2,11 @@ import requests
 import glob
 import os.path
 
-API_KEY = 'trnsl.1.1.20170627T164346Z.d63afd9e60412866.bfbdb111b490e64e1cb8454f78eb42a9b448638c'
+API_KEY = 'trnsl.1.1.20170713T222240Z.c3c095681a918beb.85b99b091d01a616dc5ff39888a2d258af0ae3f0'
 URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
 
 
-def choose_language(to_lang):
-    """
-    Возможность выбора языка для перевода
-    согласно API Yandex translate
-    параметр можем вводить через input
-    """
-    trans_lang = to_lang
-    return trans_lang
-
-
-def get_translation(text):
+def get_translation(text, lang_input):
     """
     https://translate.yandex.net/api/v1.5/tr.json/translate ?
 key=<API-ключ>
@@ -29,7 +19,7 @@ key=<API-ключ>
     params = dict(
         key=API_KEY,
         text=text,
-        lang='{}'.format(choose_language('ru')),
+        lang=lang_input,
     )
 
     response = requests.get(url=URL, params=params)
@@ -39,15 +29,13 @@ key=<API-ключ>
 def read_and_write_translation():
     files = glob.glob(os.path.join('*.txt'))
     for file in files:
-        # print('texts    ', file)
         with open(file, 'r') as f:
             text = f.read()
-            resp = get_translation(text)
-            with open('{}-{}.txt'.format(file.replace(".txt", ""), choose_language('ru')), 'w') as f:
+            text = text.encode('utf8')
+            resp = get_translation(text, lang_input='ru') # можно lang_input = input('Insert the translation\'s lang: ')
+            with open('{}-translate.txt'.format(file.replace(".txt", "")), 'w') as f:
                 res_dict = resp.json()
                 f.write(str(res_dict['text']))
                 f.close()
-        # print(str(res_dict['text']))
-
 
 read_and_write_translation()
