faa5f587736903e69ee5b63780511207c743d4bb
Netology_Pyhon <nlpythonghub@gmail.com>
2017-07-14 01:02:42 +0300

    Fixed code agreed with comments

--- Lesson2_5/homework/Homework_2_5.py
+++ Lesson2_5/homework/Homework_2_5.py
@@ -4,20 +4,16 @@ import glob
 import os.path
 
 file_copy = []
-os.makedirs('Result')
-os.makedirs('Copy_Source')
 os.chdir('Source/')
-subprocess.run('cp *.jpg ../Copy_Source', shell = True)
+subprocess.run('cp *.jpg ../Result', shell = True)
 os.chdir('../')
-files = glob.glob(os.path.join('Copy_Source/', "*.jpg"))
+files = glob.glob(os.path.join('Result/', "*.jpg"))
 for file in files:
 	print('Source   ', file)
 file_copy = files
 print(file_copy)
 for image in file_copy:
-	subprocess.call('sips --resampleWidth 200 %s' % image, shell = True)
-os.chdir('Copy_Source/')
-subprocess.run('cp *.jpg ../Result', shell = True)
-os.chdir('../Result')
+	subprocess.call('sips --resampleWidth 200 {}'.format(image), shell = True)
+os.chdir('Result/')
 subprocess.run('pwd')
-subprocess.run('ls')
+subprocess.run('ls -alt', shell = True)
