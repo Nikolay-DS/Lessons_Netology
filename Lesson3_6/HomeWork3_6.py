ddd0ad7a26721ecd21c9fcb2658776f6e546484d
Netology_Pyhon <nlpythonghub@gmail.com>
2017-07-18 19:16:22 +0300

    Homework 3_6 fixed

--- Lesson3_6/HomeWork3_6.py
+++ Lesson3_6/HomeWork3_6.py
@@ -6,77 +6,52 @@ class Animal:
 
 
 class Mammals(Animal):
-
-    def __init__(self, name, age):
-        super().__init__(name, age)
-        self.legs = 4
+    legs = 4
+            
 
     def print_mammal(self):
         print("Я млекопитающее и у меня {} лапы".format(self.legs))
 
 
 class Birds(Animal):
+    legs = 2
 
-    def __init__(self, name, age):
-        super().__init__(name, age)
-        self.legs = 2
 
     def print_bird(self):
         print("Я птица и у меня {} лапы".format(self.legs))
 
 
 class Cow(Mammals):
-
-    def __init__(self, name, age):
-        super().__init__(name, age)
-        self.voice = 'МУУУУУУУУУ'
+    voice = 'МУУУУУУУУУ'
 
 
 class Goat(Mammals):
-
-    def __init__(self, name, age):
-        super().__init__(name, age)
-        self.voice = 'Бе-е-е-е'
+    voice = 'Бе-е-е-е'
 
 
 class Sheep(Mammals):
-
-    def __init__(self, name, age):
-        super().__init__(name, age)
-        self.voice = 'М-м-м-м-б-е-е-а-а'
+    voice = 'М-м-м-м-б-е-е-а-а'
 
 
 class Pig(Mammals):
-
-    def __init__(self, name, age):
-        super().__init__(name, age)
-        self.voice = 'Хрю-хрю'
-
+    voice = 'Хрю-хрю'
+    
 
 class Duck(Birds):
-
-    def __init__(self, name, age):
-        super().__init__(name, age)
-        self.voice = 'Кря-кря'
+    voice = 'Кря-кря'
 
 
 class Chicken(Birds):
-
-    def __init__(self, name, age):
-        super().__init__(name, age)
-        self.voice = 'Ко-ко-ко'
+    voice = 'Ко-ко-ко'
 
 
 class Goose(Birds):
-
-    def __init__(self, name, age):
-        super().__init__(name, age)
-        self.voice = 'Га-га-га'
-
+    voice = 'Га-га-га'
+    
 
 cow = Cow('Буренка', 5)
 cow.print_mammal()
-print('Я {}.'.format(cow.name), 'Мне {}.'.format(cow.age), 'Я говорю {}'.format(cow.voice))
+print('Меня зовут {}.'.format(cow.name), 'Мне {}.'.format(cow.age), cow.voice)
 
 goat = Goat('Коза', 2)
 goat.print_mammal()
