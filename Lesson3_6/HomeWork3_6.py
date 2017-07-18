b41e7b2577ac0df1b6263143f578a12dcf48ddf1
Netology_Pyhon <nlpythonghub@gmail.com>
2017-07-18 19:35:33 +0300

    Homework 3_6 fixed

--- Lesson3_6/HomeWork3_6.py
+++ Lesson3_6/HomeWork3_6.py
@@ -23,33 +23,66 @@ class Birds(Animal):
 
 class Cow(Mammals):
     voice = 'МУУУУУУУУУ'
+    species = 'Корова'
+    
+    
+    def print_cow_message(self):
+        print('Меня зовут {}.'.format(self.name), 'Я - {}.'.format(self.species), 'Мне {}.'.format(self.age), '{}'.format(self.voice))
 
 
 class Goat(Mammals):
     voice = 'Бе-е-е-е'
+    species = 'Коза'
+    
+    
+    def print_cow_message(self):
+        print('Меня зовут {}.'.format(self.name), 'Я - {}.'.format(self.species), 'Мне {}.'.format(self.age), '{}'.format(self.voice))
 
 
 class Sheep(Mammals):
     voice = 'М-м-м-м-б-е-е-а-а'
+    species = 'Овца'
+    
+    
+    def print_cow_message(self):
+        print('Меня зовут {}.'.format(self.name), 'Я - {}.'.format(self.species), 'Мне {}.'.format(self.age), '{}'.format(self.voice))
 
 
 class Pig(Mammals):
     voice = 'Хрю-хрю'
+    species = 'Свинья'
     
+    
+    def print_cow_message(self):
+        print('Меня зовут {}.'.format(self.name), 'Я - {}.'.format(self.species), 'Мне {}.'.format(self.age), '{}'.format(self.voice))
 
 class Duck(Birds):
     voice = 'Кря-кря'
-
+    species = 'Утка'
+    
+    
+    def print_cow_message(self):
+        print('Меня зовут {}.'.format(self.name), 'Я - {}.'.format(self.species), 'Мне {}.'.format(self.age), '{}'.format(self.voice))
 
 class Chicken(Birds):
     voice = 'Ко-ко-ко'
-
+    species = 'Курица'
+    
+    
+    def print_cow_message(self):
+        print('Меня зовут {}.'.format(self.name), 'Я - {}.'.format(self.species), 'Мне {}.'.format(self.age), '{}'.format(self.voice))
 
 class Goose(Birds):
     voice = 'Га-га-га'
+    species = 'Гусь'
     
+    
+    def print_cow_message(self):
+        print('Меня зовут {}.'.format(self.name), 'Я - {}.'.format(self.species), 'Мне {}.'.format(self.age), '{}'.format(self.voice))   
+
 
 cow = Cow('Буренка', 5)
+cow.print_cow_message()
 cow.print_mammal()
 print('Меня зовут {}.'.format(cow.name), 'Мне {}.'.format(cow.age), cow.voice)
 
