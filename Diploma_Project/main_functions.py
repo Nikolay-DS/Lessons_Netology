378b2a4fd3df66946eaca10c2c8a1b011a0d9f45
Netology_Pyhon <nlpythonghub@gmail.com>
2017-08-14 09:59:49 +0300

    diploma particullary fixed

--- Diploma_Project/main_functions.py
+++ Diploma_Project/main_functions.py
@@ -2,91 +2,94 @@ import requests
 import time
 from termcolor import colored
 
+API = 'https://api.vk.com/method/'
 
-def get_friends(USER_ID, VERSION):
+
+def do_request(foo):
+    try:
+        foo.raise_for_status()
+        return foo
+    except requests.exceptions.Timeout as e:
+        print(e)
+        time.sleep(1)
+        foo.raise_for_status()
+        return foo
+    except Exception as e:
+        print('We are the------------{}'.format(e))
+
+
+def get_friends(user_id, version):
     params = {
-        'user_id': USER_ID,
-        'v': VERSION,
+        'user_id': user_id,
+        'v': version,
         'extended': 1,
         'fields': 'bdate'
     }
-    response = requests.get('https://api.vk.com/method/friends.get', params)
+    response = do_request(requests.get(API + 'friends.get', params))
     return response.json()
     
 
-def get_groups(USER_ID, VERSION, TOKEN):
+def get_groups(user_id, version, token):
     params = {
-        'access_token': TOKEN,
-        'user_id': USER_ID,
-        'v': VERSION,
+        'access_token': token,
+        'user_id': user_id,
+        'v': version,
         'extended': 1,
         'fields': 'members_count',
     }
-    response = requests.get('https://api.vk.com/method/groups.get', params)
+    response = do_request(requests.get(API + 'groups.get', params))
     return response.json()
 
 
-def get_friends_id_list(USER_ID, VERSION):
+def get_friends_id_list(user_id, version):
+    resp = get_friends(user_id, version)['response']  # ['items']
     try:
-        resp = get_friends(USER_ID, VERSION)['response']['items']
+        friends_id = [x['uid'] for x in resp]
     except Exception as e:
         print(e)
-        try:
-            resp = get_friends(USER_ID, VERSION)['response']
-        except Exception as e:
-            print(e)
-    try:
-        friends_id = [x['id'] for x in resp]
-    except Exception as e:
-        print(e)
-        try:
-            friends_id = [x['uid'] for x in resp]
-        except Exception as e:
-            print(e)
     print('№ of friends', len(friends_id))
     return friends_id
 
 
-def get_groups_list(USER_ID, VERSION, TOKEN):
-    resp = get_groups(USER_ID, VERSION, TOKEN)['response']['items']
+def get_groups_list(user_id, version, token):
+    resp = get_groups(user_id, version, token)['response']['items']
     my_list_groups = [x['id'] for x in resp]
     print('№ of groups {}'.format(len(my_list_groups)))
     return my_list_groups
     
 
-def get_groups_list_full(USER_ID, VERSION, TOKEN):
-    resp = get_groups(USER_ID, VERSION, TOKEN)['response']['items']
+def get_groups_list_full(user_id, version, token):
+    resp = get_groups(user_id, version, token)['response']['items']
     my_list_groups_by_id = [[x['id'], x['name'], x['members_count']] for x in resp]
     return my_list_groups_by_id
 
 
-def get_friends_groups(USER_ID, VERSION, TOKEN):
-    friends_id = get_friends_id_list(USER_ID, TOKEN)
+def get_friends_groups(user_id, version, token):
+    friends_id = get_friends_id_list(user_id, token)
     group_friends_final = []
     error_catch = []
-    friends_group_list = []
     for i, friend_id in enumerate(friends_id):
-        if (i+1) % 3 == 0:
-            time.sleep(1)
+        # if (i+1) % 3 == 0:
+        #     time.sleep(1)
         print('...{} %...'.format(round((i+1)*100/len(friends_id), 2)),
-         '№{} friend id {}'.format((i+1), friend_id))
+              '№{} friend id {}'.format((i+1), friend_id))
         try:
-            group_friends = get_groups(friend_id, VERSION, TOKEN)['response']['items']
+            group_friends = get_groups(friend_id, version, token)['response']['items']
             print('{} groups for user id {}'.format(len(group_friends), friend_id))
             print('------------------------------------------------------------------------------')
-        except Exception:
-            error_message = get_groups(friend_id, VERSION, TOKEN)['error']['error_msg']
-            print(colored('error for user id {}. The reason: {}'.format(friend_id, error_message), 'red'))
+        except Exception as e:
+            print(e)
+            error_message = get_groups(friend_id, version, token)
+            print(colored(error_message, 'red'))
+        # except Exception:
+        #     error_message = get_groups(friend_id, version, token)['error']['error_msg']
+        #     print(colored('error for user id {}. The reason: {}'.format(friend_id, error_message), 'red'))
             print('------------------------------------------------------------------------------')
-            error_catch.append(friend_id)
+        #     error_catch.append(friend_id)
         for v in group_friends:
-            if 'deactivated' in v.keys():
-                group_friends.remove(v)
-                group_friends_final.append(group_friends)
-            else:
-                group_friends_final.append(group_friends)
-    for x in group_friends_final:
-        for y in x:
-            friends_group_list.append(y['id'])
+            if 'deactivated' not in v.keys():
+                group_friends_final.append(v['id'])
+    friends_group_list = [x for x in group_friends_final]
     print(colored('Quantity of errors: {}'.format(len(error_catch)), 'red'))
+    print(set(sorted(friends_group_list)))
     return set(sorted(friends_group_list))
--- Diploma_Project/the_spy.py
+++ Diploma_Project/the_spy.py
@@ -1,21 +1,23 @@
 # -*- coding: utf-8 -*-
 
-from main_functions import *
+
+from main_functions import get_friends, get_friends_groups
+from main_functions import get_groups_list_full
 from pprint import pprint
 import json
 from termcolor import colored
 
 
 VERSION = '5.65'
-USER_ID = input('Введите ID пользователя: ')
+USER_ID = ''  # input('Введите ID пользователя: ')
 TOKEN = ''  # TOKEN
 pprint(get_friends(USER_ID, TOKEN))
 
 
-def write_to_file(USER_ID, VERSION, TOKEN):
-    user_group_id = get_groups_list(USER_ID, VERSION, TOKEN)
-    user_group_id_full = get_groups_list_full(USER_ID, VERSION, TOKEN)
-    super_final = sorted(set(user_group_id) - get_friends_groups(USER_ID, VERSION, TOKEN))
+def write_to_file(user_id, version, token):
+    user_group_id_full = get_groups_list_full(user_id, version, token)
+    user_group_id = [x[0] for x in user_group_id_full]
+    super_final = sorted(set(user_group_id) - get_friends_groups(user_id, version, token))
     output_list = []
     for x in super_final:
         for y in user_group_id_full:
@@ -25,7 +27,6 @@ def write_to_file(USER_ID, VERSION, TOKEN):
     js = json.dumps(output_list, sort_keys=True, indent=4, ensure_ascii=False, separators=(',', ': '))
     with open('groups.json', 'w+', encoding="utf-8") as f:
         f.write(js)
-        f.close()
     pprint(output_list)
     print(colored('groups.json successfully created', 'green'))
 
